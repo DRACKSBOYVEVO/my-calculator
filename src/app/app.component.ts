dde4f9c568406bab78ca531a1aeffb545eff0af5
DRACKSBOYVEVO <jjhenao48@misena.edu.co>
2021-12-29 09:16:26 -0500

    Archivos base

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-calculator';
+  title = 'Mi primera calculadora en Angular';
+
+  Numero_1:number = 0;
+  Numero_2:number = 0;
+  Resultado:number = 0;
+
+  //Metodos para utlizar la calculadora
+  Sumar():void{
+    this.Resultado = this.Numero_1 + this.Numero_2;
+  }
 }
